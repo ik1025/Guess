b598381785f64cd78812d934e1933cbedd8c5e56
ik1025 <ik1025@nyu.edu>
2021-01-24 14:21:57 -0500

    Colors

--- Guesser/Program.cs
+++ Guesser/Program.cs
@@ -10,6 +10,9 @@ namespace Guesser
             string appversion = "1.0.0";
             string appauthor = "IK";
 
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            //Console.BackgroundColor = ConsoleColor.DarkRed;
+
             Console.WriteLine("{0}: Version {1} by {2}", appName, appversion, appauthor);
         }
     }
